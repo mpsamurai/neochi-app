4df13cd1eff54ed869dbd75663c2a2a57fa9d11e
kurima-yoshida <kurima.yoshida@gmail.com>
2019-06-18 11:46:20 +0900

    RedisProivder作成途中まで

--- src/providers/redis/redis.ts
+++ src/providers/redis/redis.ts
@@ -18,6 +18,8 @@ export class RedisProvider {
   private static readonly WEBDIS_HOST = '192.168.0.20';
   private static readonly WEBDIS_PORT = 7379;
 
+  subXhrDict = {};
+
   constructor(public http: HttpClient,
     private platform: Platform) {
     console.log('Hello RedisProvider Provider');
@@ -89,10 +91,10 @@ export class RedisProvider {
     } else {
       return new Promise<string>((resolve, reject) => {
         // Webdis 経由でアクセスする
-        const url = this.getWebdisBaseUrl() + '/GET/' + key;
+        const url = this.getWebdisBaseUrl() + '/GET/' + encodeURIComponent(key) + '.txt';
         this.http.get(url).subscribe<any>(data => {
           console.log('data:', data);        
-          resolve(data.GET);
+          resolve(data);
         }, (error) => {
           reject(error);
         });
@@ -110,13 +112,84 @@ export class RedisProvider {
     return JSON.parse(stringValue);
   }
 
-  async publish(channel, message) {
+  async publish(channel: string, message: string): Promise<void> {
+    if (this.platform.is('cordova')) {
+      return new Promise<void>((resolve, reject) => {
+        cordova.plugins.Redis.publish(channel, message, 
+          (winParam) => {
+            resolve();
+          }, (error) => {
+            reject(error);
+          });
+      });      
+    } else {
+      return new Promise<void>((resolve, reject) => {
+        // Webdis 経由でアクセスする
+        const url = this.getWebdisBaseUrl() + '/PUBLISH/' + encodeURIComponent(channel) + '/' + encodeURIComponent(message);
+        this.http.get(url).subscribe<any>(data => {
+          console.log('data:', data);
+          resolve();
+        }, (error) => {
+          reject(error);
+        });
+      });
+    }    
   }
 
-  async subscribe(channel) {
+  subscribe(channel: string, success: (message: string) => void, error: (error: any) => void) {
+    if (this.platform.is('cordova')) {
+      cordova.plugins.Redis.subscribe(
+        channel,
+        (winParam) => {
+          success(winParam);
+        }, (error) => {
+          error(error);
+        });
+    } else {
+      try {
+        var previous_response_length = 0
+        const xhr = new XMLHttpRequest();
+        const url = this.getWebdisBaseUrl() + '/SUBSCRIBE/' + encodeURIComponent(channel);
+        xhr.open("GET", url, true);
+        xhr.onreadystatechange = () => {
+          if (xhr.readyState == 3)  {
+              const response = xhr.responseText;
+              const chunk = response.slice(previous_response_length);
+              previous_response_length = response.length;
+              console.log(chunk);
+              const json = JSON.parse(chunk);
+              const array = json.SUBSCRIBE;
+              if (array[0] === 'message') {
+                success(array[2]);
+              }
+            }
+        };
+        xhr.send(null);
+        this.subXhrDict[channel] = xhr;
+      } catch (e) {
+        error(e);
+      }
+    }
   }
 
-  async unsubscribe(channel) {
+  async unsubscribe(channel): Promise<void> {
+    if (this.platform.is('cordova')) {
+      return new Promise<void>((resolve, reject) => {
+        cordova.plugins.Redis.unsubscribe(
+          channel, 
+          (winParam) => {
+            resolve();
+          }, (error) => {
+            reject(error);
+          });
+      });      
+    } else {
+      if (this.subXhrDict.hasOwnProperty(channel)) {
+        const xhr = this.subXhrDict[channel];
+        delete this.subXhrDict[channel];
+        xhr.abort();
+      }
+    }
   }
 
   async finalize(): Promise<void> {
@@ -129,6 +202,13 @@ export class RedisProvider {
             reject(error);
           });
       });
+    } else {
+      for (const channel in this.subXhrDict) {
+        if (this.subXhrDict.hasOwnProperty(channel)) {
+          await this.unsubscribe(channel);
+        }
+      }
+      this.subXhrDict
     }    
   }
 
